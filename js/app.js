649d00445fb2d1881c91c284fd89f583f605a9d9
Bunny Blake <discolingua@gmail.com>
2023-07-12 20:49:21 -0400

    score counter and new art

--- js/app.js
+++ js/app.js
@@ -1,5 +1,11 @@
 const item = document.querySelector('.item');
 const boxes = document.querySelectorAll('.box');
+const basket = document.querySelector('.basketBox');
+const eggBox = document.querySelector('.eggBox');
+const scoreId = document.querySelector('.scoreBoard');
+
+var score = 0;
+
 
 boxes.forEach(box => {
     box.addEventListener('dragenter', dragEnter);
@@ -40,4 +46,17 @@ function drop(e) {
     e.target.appendChild(draggable);
 
     draggable.classList.remove('hide');
+
+    if (e.target.classList.contains('eggBox')) {
+
+        console.log("egg box");
+        eggBox.removeChild(item);
+        basket.appendChild(item);
+        score += 1;
+
+        var scoreMeter = "00000000000" + score;
+        scoreId.innerHTML = scoreMeter;
+
+
+    }
 }
